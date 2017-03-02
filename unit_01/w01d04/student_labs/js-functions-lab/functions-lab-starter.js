b4e6df1fd2ffb41246d99b66d40a8d9f8716815e
datdudejibril <datdudejibril@gmail.com>
2017-03-02 09:12:01 -0500

    changes

--- unit_01/w01d04/student_labs/js-functions-lab/functions-lab-starter.js
+++ unit_01/w01d04/student_labs/js-functions-lab/functions-lab-starter.js
@@ -1,21 +1,44 @@
+
+
 // Question 1
-function maxOfTwoNumbers() {
+//1. Define a function `maxOfTwoNumbers` that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. Do some Googling to figure this out if you forget how conditionals work.
+
+function maxOfTwoNumbers(x,y) {
+if (x < y) {
+  return = y;
+} else if (y < x) {
+  return = x;
+  }
 
 }
 
 // Question 2
-function maxOfThree() {
+//2. Define a function `maxOfThree` that takes three numbers as arguments and returns the largest of them.
 
+function maxOfThree(x,y,z) {
+  if (x > y && x > z ) {
+    return x;
+
+  } else if (y > x && y > z ) {
+    return y;
+  }
+  else (z > x && z > y ) {
+    return z;
+  }
 }
 
 // Question 3
+//3. Write a function `isCharacterAVowel` that takes a character (i.e. a string of length 1) and returns true if it is a vowel and false, otherwise.
+
 function isCharacterAVowel() {
 
 }
 
 // Question 4
+//4. Define a function `sumArray` and a function `multiplyArray` that sums and multiplies (respectively) all the numbers in an array of numbers. For example, `sumArray([1,2,3,4])` should return 10, and `multiplyArray([1,2,3,4])` should return 24.
+
 function sumArray() {
- 
+
 }
 
 
@@ -26,27 +49,35 @@ function multiplyArray() {
 
 
 // Question 5
+//5. Write a function that returns the number of arguments passed to the function when called.
+
 var numberOfArguments = function(){
-  
+
 }
 
 
 
 // Question 6
+//6. Define a function `reverseString` that reverses a string. For example, reverseString("jag testar") should return the string "ratset gaj".
+
 var reverseString = function (){
-  
+
 };
 
 
 // Question 7
+//7. Write a function `findLongestWord` that takes an array of words and returns the length of the longest word in the array.
+
 function findLongestWord () {
-  
+
 }
 
 
 // Question 8
+//8. Write a function `filterLongWords` that takes an array of words and a number `i` and returns a new array of words that are longer than `i` characters long.
+
 function filterLongWords () {
-  
+
 }
 
 
@@ -56,6 +87,6 @@ function filterLongWords () {
 
 // Bonus 2
 function charactersOccurencesCount() {
-  
+
 }
 
